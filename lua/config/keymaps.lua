6f3107bf63f2cdb237b9341a65e3692e5d3f1687
MauroLozano <mauroloz2006@gmail.com>
2026-03-09 13:03:02 -0300

    feat: add plugins and a keybind to execute java

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,15 @@
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
 
 vim.keymap.set("n", "<C-a>", "ggVG", { desc = "Select all" })
+
+--Java single file execution
+vim.keymap.set("n", "<leader>jr", function()
+  local relative_path = vim.fn.expand("%:.")
+
+  local class_path = relative_path:gsub("src/main/java/", ""):gsub("src/test/java/", ""):gsub("%.java$", "") -- Quita la extensión al final
+
+  local class_name = class_path:gsub("/", ".")
+
+  print("Ejecutando: " .. class_name)
+  vim.cmd('split | term mvn exec:java -Dexec.mainClass="' .. class_name .. '"')
+end, { desc = "Must execute current Java with maven" })
